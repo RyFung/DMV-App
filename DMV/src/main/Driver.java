5b9c976e229527d0be13884afd16528231226b90
RyFung <161082381+RyFung@users.noreply.github.com>
2024-03-24 15:23:10 -0700

    Update Driver.java

--- DMV/src/main/Driver.java
+++ DMV/src/main/Driver.java
@@ -1,5 +1,9 @@
 package main;
 
 public class Driver { //Runs DMV app
-	
+	public static void main(String args[]){
+	Input In = new Input(); //User input scanner
+	App exe = new App();
+	System.out.println(exe.run()); //Outputs string value at end of branch
+	}
 }
