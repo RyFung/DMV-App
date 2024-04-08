48f506828deb55c5f08a51636f5efd1a73d21aa6
michellee127 <164576344+michellee127@users.noreply.github.com>
2024-04-07 22:25:55 -0700

    NewReg.java

--- DMV/src/main/NewReg.java
+++ DMV/src/main/NewReg.java
@@ -1,86 +1,75 @@
+
+//inomplete
 package main;
+import java.util.Scanner;
 
-public class NewReg { //Methods behind New Registration 
+class RegistrationNode {
+    String veType;
+    RegistrationNode left, right;
 
+    public RegistrationNode(String veType) {
+        this.veType = veType;
+        this.left = this.right = null;
+    }
+}
 
-    private Input In = new Input();
+public class NewReg {
+    private RegistrationNode root;
+    Scanner input = new Scanner(System.in);
 
-    String run() {
-        String data = startNewReg(); //Set to startNewReg method
-        return data;
+    public NewReg() {
+        root = null;
     }
 
-    String startNewReg() {
-        System.out.println("Type Dealership Purchase, Motorcycle, or Commercial Vehicle");
-        String key = In.getKeys();
-        if (key.equalsIgnoreCase("Dealership Purchase")) {
-            String data = DePu();
-            return data;
-        } else if (key.equalsIgnoreCase("Moto")) {
-            String data = Moto();
-            return data;
-        } else if (key.equalsIgnoreCase("CoVe")) {
-            String data = CoVe();
-            return data;
+    public void insertVeType(String veType) { // method that inserts a new registration id
+        root = insertRec(root, veType);
+    }
+
+    private RegistrationNode insertRec(RegistrationNode root, String veType) {
+        if (root == null) {
+            root = new RegistrationNode(veType);
+            return root;
         }
-        System.out.println("Invalid option");
-        String data = startNewReg(); //Loops startNewReg() method until valid input is given
-        return data;
-
+        if (veType.compareTo(root.veType) < 0)
+            root.left = insertRec(root.left, veType);
+        else if (veType.compareTo(root.veType) > 0)
+            root.right = insertRec(root.right, veType);
+        return root;
     }
 
-    String DePu() {
-        System.out.println("Type Pass Smog Check or Fail Smog Check");
-        String key = In.getKeys();
-        if (key.equalsIgnoreCase("Pass Smog Check")) {
-            String data = "method here"; //Set to method tree for PassSC
-            return data;
-        } else if (key.equalsIgnoreCase("Fail Smog Check")) {
-            String data = endNewReg(); //End NewReg run
-            return data;
+    private boolean searchVeType(String veType) { // method to search for a registration id in bst
+        return searchRec(root, veType);
+    }
+
+    private boolean searchRec(RegistrationNode root, String veType) {
+        if (root == null) return false;
+        if (veType.equals(root.veType)) return true;
+        if (veType.compareTo(root.veType) < 0)
+            return searchRec(root.left, veType);
+        else
+            return searchRec(root.right, veType);
+    }
+
+    public void newRegistration() {
+        System.out.println("=== New Vehicle Registration ===");
+        System.out.print("What type of vehicle are you registering today?");
+        System.out.print("Type \"Deal\" for Dealership Purchase, \"Moto\" for Motorcycle, or \"Comm\" for Commercial Vehicle");
+        String veType = input.nextLine();
+
+        if (searchVeType(veType)) { // validates the user's input
+            System.out.println("Creating a new registration for vehicle type: " + veType);
+            System.out.println("New registration created successfully.");
+        } else {
+            System.out.println("Invalid input. Please try again.");
         }
-        System.out.println("Invalid option");
-        String data = DePu(); //Loops DePu() method until Pass or Fail chosen
-        return data;
     }
 
-    String Moto() {
-        System.out.println("Type Motorcycle License or No License");
-        String key = In.getKeys();
-        if (key.equalsIgnoreCase("Motorcycle License")) {
-            String data = "method here"; //Set to method tree for MotoLicense
-            return data;
-        } else if (key.equalsIgnoreCase("No License")) {
-            String data = endNewReg(); //End NewReg run
-            return data;
-        }
-        System.out.println("Invalid option");
-        String data = Moto(); //Loops Moto() method until License or No License chosen
-        return data;
+    public static void main(String[] args) {
+        NewReg newReg = new NewReg();
+        newReg.insertVeType("Deal");
+        newReg.insertVeType("Moto");
+        newReg.insertVeType("Comm");
 
+        newReg.newRegistration();
     }
-
-    String CoVe() {
-        System.out.println("Type <6000 lbs, 6000-10000 lbs, or <10000 lbs");
-        String key = In.getKeys();
-        if (key.equalsIgnoreCase("<6000 lbs")) {
-            String data = "Sorry, you need a different type of registration."; //End
-            return data;
-        } else if (key.equalsIgnoreCase("6000-10000 lbs")) {
-            String data = "method here"; //Set to method tree for LowComVe
-            return data;
-        } else if (key.equalsIgnoreCase(">10000 lbs")) {
-            String data = "method here"; //Set to method tree for HighComVe
-            return data;
-        }
-        System.out.println("Invalid option");
-        String data = CoVe(); //Loops CoVe() method until weight class chosen
-        return data;
-
-    }
-
-    String endNewReg() {
-        return "Sorry, you cannot get your New Registration today. Please come back another time.";
-    }
-
 }
